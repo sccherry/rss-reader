e7c1fb5073774dd8fc1caa23374e7e67c8d15ddf
Steve Cherry <stevenccherry@gmail.com>
2022-02-17 14:21:50 -0500

    Get rid of HTML generating files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,9 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-  eleventyConfig.addPassthroughCopy('./public');
-
   return {
     dataTemplateEngine: 'njk',
     dir: {
       input: 'src',
       output: 'dist',
-      layouts: '_layouts',
     },
   };
 };
--- src/index.md
+++
@@ -1,9 +0,0 @@
----
-layout: default
-title: Home
-eleventyNavigation:
-  key: Home
-  order: -1
----
-
-Hello world!
